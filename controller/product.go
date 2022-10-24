7c0ca17952cec4de3da7f6a68da3343bcccabe29
Maulidito Dwinandana <maulidito@gmail.com>
2022-10-24 22:11:29 +0700

    push image upload download

--- controller/product.go
+++ controller/product.go
@@ -2,6 +2,8 @@ package controller
 
 import (
 	"fmt"
+	"strings"
+	"time"
 
 	"github.com/Maulidito/personal_project_go/dataservice"
 	"github.com/Maulidito/personal_project_go/middleware"
@@ -22,6 +24,7 @@ func (ctrl *ControllerProduct) MountRouter(c fiber.Router) {
 	routerGroup := c.Group("/product")
 
 	routerGroup.Get("/", ctrl.GetAll)
+	routerGroup.Get("/get-image/:productId", ctrl.GetImageFromId)
 	routerGroup.Post("/", ctrl.middlewareAuth.Authentication, ctrl.Add)
 	routerGroup.Patch("/:productId", ctrl.middlewareAuth.Authentication, ctrl.Update)
 	routerGroup.Delete("/:productId", ctrl.middlewareAuth.Authentication, ctrl.Delete)
@@ -48,26 +51,47 @@ func (ctrl *ControllerProduct) GetOne(c *fiber.Ctx) error {
 	panic("not implemented") // TODO: Implement
 }
 
+func (ctrl *ControllerProduct) GetImageFromId(c *fiber.Ctx) error {
+	productId, err := c.ParamsInt("productId")
+	if err != nil {
+		return fiber.ErrBadRequest
+	}
+	product, err := ctrl.productDataService.GetOne(c, ctrl.db, productId)
+	if err != nil {
+		return fiber.ErrInternalServerError
+	}
+
+	if err = c.SendFile(product.Image_path); err != nil {
+		fmt.Println(err)
+	}
+	return nil
+
+}
+
 func (ctrl *ControllerProduct) Add(c *fiber.Ctx) error {
 
-	//user := c.Locals("user").(*models.User)
+	user := c.Locals("user").(*models.User)
 
 	formProduct := models.Product{}
 	if err := c.BodyParser(&formProduct); err != nil {
 		return fiber.ErrBadRequest
 	}
 
-	//fmt.Println(user)
 	image, _ := c.FormFile("image")
-	fmt.Println(image)
 
-	fmt.Println(formProduct)
+	imageF := strings.Split(image.Filename, ".")
+	image.Filename = fmt.Sprintf("%s_%s_%d.%s", imageF[0], user.Name, time.Now().Unix(), imageF[1])
 
-	// formProduct.User_Fk = user.ID
+	if err := c.SaveFile(image, fmt.Sprintf("./assets/images/%s", image.Filename)); err != nil {
+		return fiber.ErrInternalServerError
+	}
 
-	// if err := ctrl.productDataService.Add(c, ctrl.db, &formProduct); err != nil {
-	// 	return fiber.ErrInternalServerError
-	// }
+	formProduct.User_Fk = user.ID
+	formProduct.Image_path = fmt.Sprintf("assets/images/%s", image.Filename)
+
+	if err := ctrl.productDataService.Add(c, ctrl.db, &formProduct); err != nil {
+		return fiber.ErrInternalServerError
+	}
 
 	return c.SendStatus(fiber.StatusOK)
 }
